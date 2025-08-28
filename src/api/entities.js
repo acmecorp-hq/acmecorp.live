bba0951498b46e4f78b2aaffbee8b6346128bc22
Swarup Donepudi <swarup.donepudi@gmail.com>
2025-08-28 11:00:08 +0530

    added the website

---
+++ src/api/entities.js
@@ -0,0 +1,9 @@
+import { base44 } from './base44Client';
+
+
+export const TeamMember = base44.entities.TeamMember;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ src/pages/index.jsx
@@ -0,0 +1,93 @@
+import Layout from "./Layout.jsx";
+
+import Home from "./Home";
+
+import Architecture from "./Architecture";
+
+import About from "./About";
+
+import Privacy from "./Privacy";
+
+import Terms from "./Terms";
+
+import Catalog from "./Catalog";
+
+import RetailHub from "./RetailHub";
+
+import Team from "./Team";
+
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+
+const PAGES = {
+    
+    Home: Home,
+    
+    Architecture: Architecture,
+    
+    About: About,
+    
+    Privacy: Privacy,
+    
+    Terms: Terms,
+    
+    Catalog: Catalog,
+    
+    RetailHub: RetailHub,
+    
+    Team: Team,
+    
+}
+
+function _getCurrentPage(url) {
+    if (url.endsWith('/')) {
+        url = url.slice(0, -1);
+    }
+    let urlLastPart = url.split('/').pop();
+    if (urlLastPart.includes('?')) {
+        urlLastPart = urlLastPart.split('?')[0];
+    }
+
+    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
+    return pageName || Object.keys(PAGES)[0];
+}
+
+// Create a wrapper component that uses useLocation inside the Router context
+function PagesContent() {
+    const location = useLocation();
+    const currentPage = _getCurrentPage(location.pathname);
+    
+    return (
+        <Layout currentPageName={currentPage}>
+            <Routes>            
+                
+                    <Route path="/" element={<Home />} />
+                
+                
+                <Route path="/Home" element={<Home />} />
+                
+                <Route path="/Architecture" element={<Architecture />} />
+                
+                <Route path="/About" element={<About />} />
+                
+                <Route path="/Privacy" element={<Privacy />} />
+                
+                <Route path="/Terms" element={<Terms />} />
+                
+                <Route path="/Catalog" element={<Catalog />} />
+                
+                <Route path="/RetailHub" element={<RetailHub />} />
+                
+                <Route path="/Team" element={<Team />} />
+                
+            </Routes>
+        </Layout>
+    );
+}
+
+export default function Pages() {
+    return (
+        <Router>
+            <PagesContent />
+        </Router>
+    );
+}
